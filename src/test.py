9c7c6a686e787feed81e1383ca774d1a6daa77c9
Ryan <ry4n7501@gmail.com>
2024-10-13 12:12:28 -0400

    - Created read_file branch for testing code to read the file - Reads file, uses Counter to count word frequencies

--- src/test.py
+++ src/test.py
@@ -1,2 +1,23 @@
+from collections import Counter
 import pandas as pd
+import re
 
+
+#String to store all reviews
+lines = ""
+
+# Open and read the file, specify encoding because I get an error otherwise
+with open('reviews.txt', 'r', encoding='utf-8') as f:
+    # Read the lines, convert to lowercase, store in lines variable
+    lines = f.read().lower() # Note: append .lower() to have all words in lines be lowercase
+    #print(type(lines))
+    print(lines)
+
+#Create a dict for storing all unique words. Note: maybe create two Counters; one for incrementing when a word appears in a postivie review, another for when a word appears in a negative review
+wordCount = Counter(lines.split())
+print(wordCount)
+# Populating words dict with unique words and their frequencies
+# for x in lines:
+#     words[x] += 1
+#
+# print(words.most_common(10))
